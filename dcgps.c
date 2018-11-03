7136aecd4c43147a2e808186b34c1fd2f09900c8
Keishi Asai <isheik808@gmail.com>
2018-11-03 16:21:57 -0700

    test

--- dcgps.c
+++ dcgps.c
@@ -32,8 +32,8 @@ int main()
   source.device = NULL;
 
   /* note: we're assuming BSD-style reliable signals here */
-  (void)signal(SIGINT, die);
-  (void)signal(SIGHUP, die);
+  // (void)signal(SIGINT, die);
+  // (void)signal(SIGHUP, die);
   // (void)signal(SIGWINCH, resize);
 
   if (gps_open(source.server, source.port, &gpsdata) != 0)
