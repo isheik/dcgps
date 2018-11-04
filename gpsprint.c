54cf292f242c92dff4cf08e57c8969d80f486db4
Keishi Asai <isheik808@gmail.com>
2018-11-04 13:23:12 -0800

    test

---
+++ gpsprint.c
@@ -0,0 +1,41 @@
+#include <gps.h>
+#include <math.h>
+#include "gpsprint.h"
+#include "gpsdclient.h"
+
+void gps_print(struct gps_data_t *gpsdata)
+{
+  // Print gps
+  for (i = 0; i < MAXCHANNELS; i++)
+  {
+    usedflags[i] = false;
+    for (j = 0; j < gpsdata->satellites_used; j++)
+      if (gpsdata->skyview[j].used == gpsdata->skyview[j].PRN)
+        usedflags[i] = true;
+  }
+
+  if (gpsdata->satellites_visible != 0) // Some satellites are visible {
+  {
+    for (i = 0; i < MAX_POSSIBLE_SATS; i++)
+    {
+      if (i < gpsdata->satellites_visible)
+      {
+        // Print out parameters for each visible satellite {
+        // fprintf (stdout, "PRN: %3d gpsdata->PRN[i],
+        // .............................
+        // .............................);
+        if (gpsdata->fix.mode >= MODE_2D && isnan(gpsdata->fix.latitude) == 0)
+        {
+          fprintf(stdout, "Latitude: %s %c;", deg_to_str(deg_type, fabs(gpsdata->fix.latitude)), (gpsdata->fix.latitude < 0) ? 'S' : 'N');
+          fflush(stdout);
+        }
+        else
+          printf("n/a\n");
+      }
+    }
+  }
+  else
+  {
+    fprintf(stderr, "No satellite found\n");
+  }
+}
--- gpsprint.h
+++ gpsprint.h
@@ -1,4 +1,6 @@
 #ifndef GPS_PRINT
 #define GPS_PRINT
 
+void gps_print(struct gps_data_t *gpsdata);
+
 #endif
