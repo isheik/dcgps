ed92f51ac3990c5bcc6c3ecebe2ecdea74b8e070
Keishi Asai <isheik808@gmail.com>
2018-11-03 20:55:25 -0700

    test

--- gps-utils.c
+++ gps-utils.c
@@ -46,8 +46,9 @@
 //   /* Bye! */
 //   exit(EXIT_SUCCESS);
 // }
+bool usedflags[MAXCHANNELS];
 
-void* gps_loop(void *gpsdata)
+void *gps_loop(void *gpsdata)
 {
   int wait_clicks = 0;
   while (1)
@@ -76,7 +77,38 @@ void* gps_loop(void *gpsdata)
       }
       else
       {
-        fprintf(stderr, "things going well\n");
+        for (i = 0; i < MAXCHANNELS; i++)
+        {
+          usedflags[i] = false;
+          for (j = 0; j < gpsdata->satellites_used; j++)
+            if (gpsdata->used[j] == gpsdata->PRN[i])
+              usedflags[i] = true;
+        }
+
+        if (gpsdata->satellites_visible != 0) // Some satellites are visible {
+        {
+          for (i = 0; i < MAX_POSSIBLE_SATS; i++)
+          {
+            if (i < gpsdata->satellites_visible)
+            {
+              // Print out parameters for each visible satellite {
+              // fprintf (stdout, "PRN: %3d gpsdata->PRN[i],
+              // .............................
+              // .............................);
+              if (gpsdata->fix.mode >= MODE_2D && isnan(gpsdata->fix.latitude) == 0)
+              {
+                fprintf(stdout, "Latitude: %s %c;", deg_to_str(deg_type, fabs(gpsdata->fix.latitude)), (gpsdata->fix.latitude < 0) ? 'S' : 'N');
+                fflush(stdout);
+              }
+              else
+                printf("n/a\n");
+            }
+          }
+        }
+        else
+        {
+          fprintf(stderr, "No satellite found\n");
+        }
       }
     }
   }
