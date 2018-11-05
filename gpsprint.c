d235b09a258cf067da04332b562ed44e62610296
Keishi Asai <isheik808@gmail.com>
2018-11-04 16:46:13 -0800

    test

--- gpsprint.c
+++ gpsprint.c
@@ -37,7 +37,7 @@ void gps_print(struct gps_data_t *gpsdata)
         fprintf(stdout, "PRN: %3d ", gpsdata->skyview[i].PRN);
         fprintf(stdout, "Elevation: %02d ", gpsdata->skyview[i].elevation);
         fprintf(stdout, "Azimuth: %03d ", gpsdata->skyview[i].azimuth);
-        fprintf(stdout, "SNR: %02f ", gpsdata->skyview[i].ss);
+        fprintf(stdout, "SNR: %02f ", (int)gpsdata->skyview[i].ss);
         fprintf(stdout, "Used %c\n", (gpsdata->skyview[i].used) ? 'Y' : 'N');
         // fprintf(stdout, "PRN: %3d Elevation: %02d Azimuth: %03d SNR: %02f Used %c\n",
         // gpsdata->skyview[i].PRN, gpsdata->skyview[i].elevation, gpsdata->skyview[i].azimuth, gpsdata->skyview[i].ss, (gpsdata->skyview[i].used) ? 'Y' : 'N');
@@ -59,7 +59,7 @@ void gps_print(struct gps_data_t *gpsdata)
     strftime(date_str, 256, "%Y-%m-%dT%H:%M:%S.000Z; ", update_time_st);
     fputs(date_str, stdout);
     fprintf(stdout, "Latitude: %f %c ; ", gpsdata->fix.latitude, (gpsdata->fix.latitude < 0) ? 'S' : 'N');
-    fprintf(stdout, "Longitude: %f %c ;\n\n", gpsdata->fix.longitude, (gpsdata->fix.longitude < 0) ? 'W' : 'E');
+    fprintf(stdout, "Longitude: %f %c\n\n", gpsdata->fix.longitude, (gpsdata->fix.longitude < 0) ? 'W' : 'E');
     // fprintf(stdout, "%f ; Latitude: %f %c ; Longitude: %f %c",
     //         (double)update_time, gpsdata->fix.latitude,
     //         (gpsdata->fix.latitude < 0) ? 'S' : 'N',
